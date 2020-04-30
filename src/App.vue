009b82f1c3edc3970e9d3222b03d9c6a09f6310b
Henri Delozanne <hnr.delozanne@gmail.com>
2020-04-30 11:25:13 +0200

    Initialize i18n.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ $t('whoAmI') }}</h1>
+    <div class="btn-ctn">
+      <button @click="setLanguage('en')">{{ $t('languages.english') }}</button>
+      <button @click="setLanguage('fr')">{{ $t('languages.french') }}</button>
+      <button @click="setLanguage('es')">{{ $t('languages.spanish') }}</button>
+    </div>
+    <span class="current">{{ $t('current') }} : {{ currentLanguage }}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import i18n from './i18n';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      currentLanguage: 'en',
+    };
+  },
+  methods: {
+    setLanguage(language) {
+      this.currentLanguage = language;
+      i18n.locale = language;
+    },
   },
 };
 </script>
@@ -24,5 +37,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.btn-ctn {
+  width: 70%;
+  margin: 100px auto;
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  padding: 5px 10px;
+  background: teal;
+  color: white;
+  font-size: 18px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
